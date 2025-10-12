6eaf1cae6162ef2a33e8fd01a6ae42b1a66c077e
IlyaSuponev <suponeff.i@gmail.com>
2025-10-12 14:04:00 +0300

    fix: navigation
    Add check for navigation to current screen
    
    Signed-off-by: IlyaSuponev <suponeff.i@gmail.com>

--- composeApp/src/commonMain/kotlin/com/isuponev/tutordb/core/views/screens/Navigation.kt
+++ composeApp/src/commonMain/kotlin/com/isuponev/tutordb/core/views/screens/Navigation.kt
@@ -2,4 +2,7 @@ package com.isuponev.tutordb.core.views.screens
 
 import androidx.navigation.NavHostController
 
-fun NavHostController.navigate(target: Screen) = navigate(target.route)
+fun NavHostController.navigate(target: Screen) {
+    if (this.currentDestination?.route == target.route) return
+    navigate(target.route)
+}
