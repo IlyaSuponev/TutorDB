6a7cf520684837c9ef62c5f36820c34edf3885b5
IlyaSuponev <suponeff.i@gmail.com>
2025-10-12 14:07:21 +0300

    feat: Main
    Add logging about application start and finish.
    Add correct closing for AppConfig
    
    Signed-off-by: IlyaSuponev <suponeff.i@gmail.com>

--- composeApp/src/jvmMain/kotlin/com/isuponev/tutordb/desktop/main.kt
+++ composeApp/src/jvmMain/kotlin/com/isuponev/tutordb/desktop/main.kt
@@ -2,17 +2,26 @@ package com.isuponev.tutordb.desktop
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.isuponev.tutordb.core.config.AppConfig
+import com.isuponev.tutordb.core.logging.appLogger
 import com.isuponev.tutordb.core.resources.SharedResources
 import com.isuponev.tutordb.core.views.App
 
 /**
  * Point of program start.
  */
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = SharedResources.strings.fullAppName.localized(),
-    ) {
-        App()
+fun main() {
+    appLogger.i { "App has been started" }
+    application {
+        Window(
+            onCloseRequest = {
+                appLogger.i { "App has been closed" }
+                AppConfig.close()
+                exitApplication()
+            },
+            title = SharedResources.strings.fullAppName.localized(),
+        ) {
+            App()
+        }
     }
 }
